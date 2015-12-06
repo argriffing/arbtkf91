d79f1ff4fb50096fc76f11c7b4a23ea6982f4eed
alex <argriffi@ncsu.edu>
2015-12-05 20:24:21 -0500

    try to understand how integer lattices work

--- iface.c
+++ iface.c
@@ -2,20 +2,17 @@
  * -I/usr/local/include/flint -lflint
  */
 
-/*
---sequence-1 ACGACTAGTCAGCTACGATCGACTCATTCAACTGACTGACATCGACTTA
---sequence-2 AGAGAGTAATGCATACGCATGCATCTGCTATTCTGCTGCAGTGGTA
---lambda 1 --mu 2 --tau 0.1 --pa 0.25 --pc 0.25 --pg 0.25 --pt 0.25
-*/
-
 /* this is kind of ridiculous... */
 /* but parsing sucks so I want to be super explicit */
 /*
+--sequence-1 ACGACTAGTCAGCTACGATCGACTCATTCAACTGACTGACATCGACTTA
+--sequence-2 AGAGAGTAATGCATACGCATGCATCTGCTATTCTGCTGCAGTGGTA
 --lambda-num 1 --lambda-den 1 --mu-num 2 --mu-den 1 --tau-num 1 --tau-den 10 --pa-num 1 --pa-den 4 --pc-num 1 --pc-den 4 --pg-num 1 --pg-den 4 --pt-num 1 --pt-den 4
 */
 
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 #include "flint.h"
@@ -83,15 +80,15 @@ main(int argc, char *argv[])
     user_params_t p;
     user_params_init(p);
 
-    slong lambda_num = 0;
-    slong mu_num = 0;
-    slong tau_num = 0;
-    slong pi_num[4] = {0, 0, 0, 0};
+    ulong lambda_num = 0;
+    ulong mu_num = 0;
+    ulong tau_num = 0;
+    ulong pi_num[4] = {0, 0, 0, 0};
 
-    slong lambda_den = 1;
-    slong mu_den = 1;
-    slong tau_den = 1;
-    slong pi_den[4] = {1, 1, 1, 1};
+    ulong lambda_den = 1;
+    ulong mu_den = 1;
+    ulong tau_den = 1;
+    ulong pi_den[4] = {1, 1, 1, 1};
 
     for (i = 1; i < argc-1; i += 2)
     {
@@ -102,44 +99,44 @@ main(int argc, char *argv[])
         }
         
         else if (strcmp(argv[i], "--lambda-num") == 0) {
-            flint_sscanf(argv[i + 1], "%ws", &lambda_num);
+            flint_sscanf(argv[i + 1], "%wu", &lambda_num);
         } else if (strcmp(argv[i], "--mu-num") == 0) {
-            flint_sscanf(argv[i + 1], "%ws", &mu_num);
+            flint_sscanf(argv[i + 1], "%wu", &mu_num);
         } else if (strcmp(argv[i], "--tau-num") == 0) {
-            flint_sscanf(argv[i + 1], "%ws", &tau_num);
+            flint_sscanf(argv[i + 1], "%wu", &tau_num);
         } else if (strcmp(argv[i], "--pa-num") == 0) {
-            flint_sscanf(argv[i + 1], "%ws", pi_num+0);
+            flint_sscanf(argv[i + 1], "%wu", pi_num+0);
         } else if (strcmp(argv[i], "--pc-num") == 0) {
-            flint_sscanf(argv[i + 1], "%ws", pi_num+1);
+            flint_sscanf(argv[i + 1], "%wu", pi_num+1);
         } else if (strcmp(argv[i], "--pg-num") == 0) {
-            flint_sscanf(argv[i + 1], "%ws", pi_num+2);
+            flint_sscanf(argv[i + 1], "%wu", pi_num+2);
         } else if (strcmp(argv[i], "--pt-num") == 0) {
-            flint_sscanf(argv[i + 1], "%ws", pi_num+3);
+            flint_sscanf(argv[i + 1], "%wu", pi_num+3);
         }
 
         else if (strcmp(argv[i], "--lambda-den") == 0) {
-            flint_sscanf(argv[i + 1], "%ws", &lambda_den);
+            flint_sscanf(argv[i + 1], "%wu", &lambda_den);
         } else if (strcmp(argv[i], "--mu-den") == 0) {
-            flint_sscanf(argv[i + 1], "%ws", &mu_den);
+            flint_sscanf(argv[i + 1], "%wu", &mu_den);
         } else if (strcmp(argv[i], "--tau-den") == 0) {
-            flint_sscanf(argv[i + 1], "%ws", &tau_den);
+            flint_sscanf(argv[i + 1], "%wu", &tau_den);
         } else if (strcmp(argv[i], "--pa-den") == 0) {
-            flint_sscanf(argv[i + 1], "%ws", pi_den+0);
+            flint_sscanf(argv[i + 1], "%wu", pi_den+0);
         } else if (strcmp(argv[i], "--pc-den") == 0) {
-            flint_sscanf(argv[i + 1], "%ws", pi_den+1);
+            flint_sscanf(argv[i + 1], "%wu", pi_den+1);
         } else if (strcmp(argv[i], "--pg-den") == 0) {
-            flint_sscanf(argv[i + 1], "%ws", pi_den+2);
+            flint_sscanf(argv[i + 1], "%wu", pi_den+2);
         } else if (strcmp(argv[i], "--pt-den") == 0) {
-            flint_sscanf(argv[i + 1], "%ws", pi_den+3);
+            flint_sscanf(argv[i + 1], "%wu", pi_den+3);
         }
     }
 
-    fmpq_set_si(p->lambda, lambda_num, lambda_den);
-    fmpq_set_si(p->mu, mu_num, mu_den);
-    fmpq_set_si(p->tau, tau_num, tau_den);
+    fmpq_set_ui(p->lambda, lambda_num, lambda_den);
+    fmpq_set_ui(p->mu, mu_num, mu_den);
+    fmpq_set_ui(p->tau, tau_num, tau_den);
     for (i = 0; i < 4; i++)
     {
-        fmpq_set_si(p->pi+i, pi_num[i], pi_den[i]);
+        fmpq_set_ui(p->pi+i, pi_num[i], pi_den[i]);
     }
 
     user_params_print(p);
