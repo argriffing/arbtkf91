ef819e9c461ac3ea3f00974c03849588622a84e0
alex <argriffi@ncsu.edu>
2016-01-02 10:29:29 -0500

    use a more abstract interface to the dynamic programming, and remove an unused python script

---
+++ tkf91_dp.c
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+
+#include "flint/flint.h"
+#include "flint/fmpz.h"
+
+#include "arb.h"
+
+#include "tkf91_dp.h"
+
+void
+solution_init(solution_t x, slong aln_maxlen)
+{
+    x->A = flint_malloc(sizeof(char) * (aln_maxlen+1));
+    x->B = flint_malloc(sizeof(char) * (aln_maxlen+1));
+    x->len = -1;
+    arb_init(x->log_probability);
+    fmpz_init(x->best_tie_count);
+    x->has_best_tie_count = 0;
+    x->optimality_flag = 0;
+}
+
+void
+solution_clear(solution_t x)
+{
+    flint_free(x->A);
+    flint_free(x->B);
+    arb_clear(x->log_probability);
+    fmpz_clear(x->best_tie_count);
+}
+
+void
+solution_fprint(FILE * file, const solution_t x)
+{
+    /* print the two sequences */
+    flint_fprintf(file, "alignment:\n");
+    if (x->A)
+    {
+        flint_fprintf(file, "%s\n", x->A);
+    }
+    if (x->B)
+    {
+        flint_fprintf(file, "%s\n", x->B);
+    }
+
+    /* report what is known about the optimality of the solution */
+    flint_fprintf(file, "alignment optimality : ");
+    if (x->optimality_flag)
+    {
+        flint_fprintf(file, "verified");
+    }
+    else
+    {
+        flint_fprintf(file, "unknown");
+    }
+    flint_fprintf(file, "\n");
+
+    /* report the number of ties */
+    flint_fprintf(file, "number of optimal alignments : ");
+    if (x->optimality_flag && x->has_best_tie_count)
+    {
+        fmpz_fprint(file, x->best_tie_count);
+    }
+    else
+    {
+        flint_fprintf(file, "unknown");
+    }
+    flint_fprintf(file, "\n");
+
+    /* report the log probability of the best alignment */
+    flint_fprintf(file, "alignment log probability : ");
+    arb_fprintd(file, x->log_probability, 15);
+    flint_fprintf(file, "\n");
+    flint_fprintf(file, "... and in a more nerdy format : ");
+    arb_fprint(file, x->log_probability);
+    flint_fprintf(file, "\n");
+}
---
+++ tkf91_dp.h
@@ -0,0 +1,79 @@
+#ifndef TKF91_DP_H
+#define TKF91_DP_H
+
+/*
+ * This describes the common interface
+ * for tkf91 dynamic programming solvers.
+ */
+
+#include <stdio.h>
+#include <time.h>
+
+#include "flint/flint.h"
+#include "flint/fmpz_mat.h"
+
+#include "expressions.h"
+#include "tkf91_generator_indices.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Define the data structure that is used for the output.
+ * The construction and destruction is defined and performed
+ * in the arbtk91.c module; the specific tkf91 dynamic programming
+ * solvers themselves do not need to know about those functions.
+ */
+typedef struct
+{
+    char * A;
+    char * B;
+    slong len;
+    arb_t log_probability;
+    fmpz_t best_tie_count;
+    int has_best_tie_count;
+    int optimality_flag;
+} solution_struct;
+typedef solution_struct solution_t[1];
+
+void solution_init(solution_t sol, slong aln_maxlen);
+void solution_clear(solution_t sol);
+void solution_fprint(FILE * file, const solution_t sol);
+
+static __inline__ void
+solution_print(const solution_t x)
+{
+    solution_fprint(stdout, x);
+}
+
+
+/*
+ * Define the data structure to be used for miscellaneous request options.
+ * The png filename is NULL if no tableau visualization is requested.
+ * The trace option is nonzero if the actual alignment is requested (as
+ * opposed to requesting just the best score).
+ * The nrepeats option may be greater than 1 for the purposes of benchmarking.
+ */
+typedef struct
+{
+    char * png_filename;
+    int trace;
+} request_struct;
+typedef request_struct request_t[1];
+
+
+/* function pointer typedef for the dynamic programming function */
+typedef void (*tkf91_dp_fn)(
+        solution_t, const request_t,
+        fmpz_mat_t, expr_ptr *, tkf91_generator_indices_t,
+        slong *, size_t,
+        slong *, size_t);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
