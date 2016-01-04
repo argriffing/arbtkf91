12f2beaccd99483867caaa9534c3aaadc75b7b26
alex <argriffi@ncsu.edu>
2016-01-04 15:19:29 -0500

    check what proportions of alignments found by published code are optimal/canonical

--- repro.py
+++ repro.py
@@ -13,26 +13,49 @@ from data_source import gen_files, gen_sequence_pairs
 
 
 def align_pair(align, model_params, a, b):
-    d = model_params.copy()
-    d.update({"sequence-1" : a, "sequence-2" : b})
+    d = model_params[:]
+    d.extend([
+        ("sequence-1", a),
+        ("sequence-2", b)])
     args = [align]
-    for k, v in d.items():
+    for k, v in d:
         args.extend(['--'+k, v])
+    print(args)
     p = Popen(args, stdout=PIPE, stdin=PIPE, stderr=PIPE)
     return p.communicate()
 
 
 def main():
     align = os.path.realpath('../../stamatakis/tkf91_scaling/tkf91')
-    model_params = {
-            "pa" : "0.27",
-            "pc" : "0.24",
-            "pg" : "0.26",
-            "pt" : "0.23",
-            "lambda" : "1",
-            "mu" : "2",
-            "tau" : "0.1",
-            }
+    model_params = [
+            ("pa", "0.27"),
+            ("pc", "0.24"),
+            ("pg", "0.26"),
+            ("pt", "0.23"),
+            ("lambda", "1"),
+            ("mu", "2"),
+            ("tau", "0.1"),
+            ]
+    #a = 'ACGACTAGTCAGCTACGATCGACTCATTCAACTGACTGACATCGACTTA'
+    #b = 'AGAGAGTAATGCATACGCATGCATCTGCTATTCTGCTGCAGTGGTA'
+    #a = a*10
+    #b = b*10
+    # 45 ok
+    # 46 fail
+    # 47
+    # 48
+    # 49
+    # 50 fail
+    n = 45
+    a = 'A'*n
+    b = 'C'*n
+    out, err = align_pair(align, model_params, a, b)
+    if 'failed' in err:
+        print('it seemed to fail with the error message')
+        print(err)
+    else:
+        print('it seemed to work')
+    return
     for name, fin in gen_files():
         print(name)
         with open("out-" + name, "wt") as fout:
---
+++ repro2.py
@@ -0,0 +1,93 @@
+"""
+Run programs that other people have written.
+
+For this one, it looks like the output can be parsed by looking for the line
+that says 'log_caching_round_up' and then the next two lines are the
+nucleotide sequences.
+
+"""
+from __future__ import print_function, division
+
+from StringIO import StringIO
+from subprocess import Popen, PIPE
+import os
+import json
+
+from data_source import gen_files, gen_sequence_pairs
+
+
+align = os.path.realpath(os.path.join(
+    '..', 'bioinf2015', 'implementation-team2',
+    'bin', 'TKFLOG_CACHING_ROUND_UP'))
+
+check = os.path.realpath('./bin/arbtkf91-check')
+
+
+def align_pair(a, b):
+    pairs = [
+        ("output-alignment", "1"),
+        ("pa", "0.27"),
+        ("pc", "0.24"),
+        ("pg", "0.26"),
+        ("pt", "0.23"),
+        ("lambda", "1"),
+        ("mu", "2"),
+        ("tau", "0.1"),
+        ("sequence-1", a),
+        ("sequence-2", b),
+        ]
+    args = [align]
+    for k, v in pairs:
+        args.extend(['--'+k, v])
+    p = Popen(args, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    return p.communicate()
+
+
+def check_pair(a, b):
+    model_params = dict(
+            pa_n=27, pa_d=100,
+            pc_n=24, pc_d=100,
+            pg_n=26, pg_d=100,
+            pt_n=23, pt_d=100,
+            lambda_n=1, lambda_d=1,
+            mu_n=2, mu_d=1,
+            tau_n=1, tau_d=10)
+    d = model_params.copy()
+    d.update(sequence_a=a, sequence_b=b)
+    s_in = json.dumps(d)
+    args = [check]
+    p = Popen(args, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    data = p.communicate(input=s_in)
+    stdout_data, stderr_data = data
+    return json.loads(stdout_data)
+
+
+def main():
+    for name, fin in gen_files():
+        ncanon = 0
+        nopt = 0
+        k = 0
+        for a, b in gen_sequence_pairs(fin, force_acgt=True):
+            out, err = align_pair(a, b)
+            lines = [x.strip() for x in out.splitlines()]
+            nlines = len(lines)
+            for i in range(nlines):
+                if lines[i] == 'log_caching_round_up':
+                    a_aln = lines[i+1]
+                    b_aln = lines[i+2]
+                    break
+            js = check_pair(a_aln, b_aln)
+            if js['alignment_is_canonical'] == 'yes':
+                ncanon += 1
+            if js['alignment_is_optimal'] == 'yes':
+                nopt += 1
+            k += 1
+        print(name)
+        print('total number of alignments:', k)
+        print('number of optimal alignments:', nopt)
+        print('number that are also canonical:', ncanon)
+        print()
+
+
+if __name__ == '__main__':
+    main()
