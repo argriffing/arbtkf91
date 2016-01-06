e7ca47cae9df0f8b65482d2c2cf20136895eea9b
alex <argriffi@ncsu.edu>
2016-01-05 20:45:26 -0500

    update the python benchmarking script for the new interface

--- bench-analysis.py
+++ bench-analysis.py
@@ -6,33 +6,22 @@ import json
 
 import data_source
 
+def rat(a, b):
+    return dict(num=a, denom=b)
 
 def bench_pair(bench, model_params, a, b):
-    d = model_params.copy()
-    d.update(precision='float', samples=10, sequence_a=a, sequence_b=b)
+    d = dict(
+        precision='float',
+        samples=10,
+        parameters=model_params,
+        sequence_a=a,
+        sequence_b=b)
     s_in = json.dumps(d)
     args = [bench]
     p = Popen(args, stdout=PIPE, stdin=PIPE, stderr=PIPE)
     data = p.communicate(input=s_in)
     stdout_data, stderr_data = data
-    try:
-        out = json.loads(stdout_data)
-    except ValueError as e:
-        print('raw stdout data:')
-        print(stdout_data)
-        print('raw stderr data:')
-        print(stderr_data)
-        raise
-    """
-    print('verified:', out['verified'])
-    if not out['verified']:
-        print('input and output corresponding to verification failure:')
-        print(s_in)
-        print(stdout_data)
-        raise Exception('verification failed')
-    """
-    #print('json stuff:')
-    #print(out)
+    out = json.loads(stdout_data)
     elapsed = out['elapsed_ticks']
     print(elapsed)
     return sum(elapsed)
@@ -40,14 +29,14 @@ def bench_pair(bench, model_params, a, b):
 
 def main():
     bench = os.path.realpath('./bin/arbtkf91-bench')
-    model_params = dict(
-            pa_n=27, pa_d=100,
-            pc_n=24, pc_d=100,
-            pg_n=26, pg_d=100,
-            pt_n=23, pt_d=100,
-            lambda_n=1, lambda_d=1,
-            mu_n=2, mu_d=1,
-            tau_n=1, tau_d=10)
+    model_params = {
+            "pa" : rat(27, 100),
+            "pc" : rat(24, 100),
+            "pg" : rat(26, 100),
+            "pt" : rat(23, 100),
+            "lambda" : rat(1, 1),
+            "mu" : rat(2, 1),
+            "tau" : rat(1, 10)}
     for name, fin in data_source.gen_files():
         print(name)
         total_ticks = 0
