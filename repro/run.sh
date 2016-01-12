6b35c6f8d5c4afb0a94ca27f9d14494b6bb5f9e0
alex <argriffi@ncsu.edu>
2016-01-11 20:06:35 -0500

    rename the examples directory

---
+++ repro/run.sh
@@ -0,0 +1,20 @@
+BENCH_DATA="../../../stamatakis/benchMark_data"
+TEAM1="../../bioinf2015/implementation-team1/build/mlalign"
+TEAM2="../../bioinf2015/implementation-team2/bin/TKFLOG_CACHING_ROUND_UP"
+REF="../../../stamatakis/tkf91_scaling/tkf91"
+
+CMD="python repro.py --ref-exe=$REF"
+echo $CMD
+$CMD
+
+CMD="python repro2.py --exe=$TEAM2 --bench-data=$BENCH_DATA"
+echo $CMD
+$CMD
+
+CMD="python repro3.py --exe=$TEAM1 --bench-data=$BENCH_DATA"
+echo $CMD
+$CMD
+
+CMD="python repro4.py --precision=float --bench-data=$BENCH_DATA"
+echo $CMD
+$CMD
